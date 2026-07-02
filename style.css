b7a70373b5124b0f3103cdafc319b2d0608ead0c
mazurheloisa-ui <mazur.heloisa@escola.pr.gov.br>
2026-07-02 13:56:57 -0300

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,134 @@
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:Arial, Helvetica, sans-serif;
+}
 
+body{
+
+    background:#b8ece7;
+
+    display:flex;
+    justify-content:center;
+    align-items:center;
+
+    height:100vh;
+}
+
+.calculator{
+
+    width:330px;
+
+    background:#8fe2db;
+
+    padding:20px;
+
+    border-radius:20px;
+
+    box-shadow:
+    inset 5px 5px 10px rgba(255,255,255,.5),
+    inset -5px -5px 10px rgba(0,0,0,.12),
+    10px 15px 30px rgba(0,0,0,.18);
+
+}
+
+.brand{
+
+    display:flex;
+    justify-content:space-between;
+    margin-bottom:10px;
+    color:white;
+}
+
+.brand span{
+
+    font-size:30px;
+    font-weight:bold;
+
+}
+
+.brand small{
+
+    align-self:flex-end;
+}
+
+#display{
+
+    width:100%;
+    height:75px;
+
+    border:none;
+
+    border-radius:10px;
+
+    background:#d7e3d0;
+
+    font-size:42px;
+
+    text-align:right;
+
+    padding-right:15px;
+
+    margin-bottom:20px;
+
+    color:#303030;
+
+    box-shadow:
+    inset 3px 3px 8px rgba(0,0,0,.2);
+}
+
+.buttons{
+
+    display:grid;
+
+    grid-template-columns:repeat(4,1fr);
+
+    gap:12px;
+}
+
+button{
+
+    height:58px;
+
+    border:none;
+
+    border-radius:16px;
+
+    background:#95e5df;
+
+    font-size:24px;
+
+    color:white;
+
+    cursor:pointer;
+
+    box-shadow:
+    5px 5px 10px rgba(0,0,0,.12),
+    inset 2px 2px 5px rgba(255,255,255,.6);
+
+    transition:.15s;
+}
+
+button:hover{
+
+    transform:translateY(-2px);
+
+}
+
+button:active{
+
+    transform:scale(.95);
+
+}
+
+.zero{
+
+    grid-column:span 2;
+}
+
+.plus{
+
+    grid-row:span 2;
+    height:128px;
+}
